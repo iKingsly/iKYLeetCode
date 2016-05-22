75a680024a33d0fe69429abdca795c103499dbe8
郑钦洪 <369309988@qq.com>
2016-05-22 17:16:51 +0800

    提交第67题 二进制数相加

---
+++ 67. Add Binary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+class Solution(object):
+	    def addBinary(self, a, b):
+		"""
+		:type a: str
+		:type b: str
+		:rtype: str
+		"""
+		return bin(int(a,2)+int(b,2))[2:]
+		
+		
+c = Solution().addBinary("1111", "1")
+print(c)
