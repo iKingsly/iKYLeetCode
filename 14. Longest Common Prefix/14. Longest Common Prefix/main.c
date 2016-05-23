1318bc8299619f507cb8de7daf671f1e5ae6c44a
郑钦洪 <rokingsly@gmail.com>
2016-05-23 21:48:19 +0800

    14题的C语言解法

--- 14. Longest Common Prefix/14. Longest Common Prefix/main.c
+++ 14. Longest Common Prefix/14. Longest Common Prefix/main.c
@@ -7,7 +7,36 @@
 //
 
 #include <stdio.h>
-
+#include <string.h>
+#include <stdlib.h>
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strs == NULL || *strs == NULL)
+        return "";
+    
+    int len = (int)strlen(strs[0]);
+    char *ps = strs[0], *rest;
+    
+    int max = 0;
+    
+    while (max < len) {
+        for (int i = 1; i < strsSize; i++) {
+            if (strs[i][max] != ps[max]) {
+                len = max;
+                break;
+            }
+        }
+        if (max < len) {
+            max++;
+        }
+    }
+    
+    rest = malloc(sizeof(char) * (max + 1));
+    for (int i = 0; i < max; i++) {
+        rest[i]=strs[0][i];
+    }
+    rest[max] = '\0';
+    return rest;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
