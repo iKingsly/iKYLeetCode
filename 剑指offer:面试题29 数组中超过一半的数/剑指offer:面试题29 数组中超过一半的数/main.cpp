b47511a07ed641a25800a4d0487dd953622c2602
郑钦洪 <369309988@qq.com>
2016-06-16 22:38:55 +0800

    剑指offer:面试题29 数组中超过一半的数

--- 剑指offer:面试题29 数组中超过一半的数/剑指offer:面试题29 数组中超过一半的数/main.cpp
+++ 剑指offer:面试题29 数组中超过一半的数/剑指offer:面试题29 数组中超过一半的数/main.cpp
@@ -7,7 +7,25 @@
 //
 
 #include <iostream>
-
+int MoreThanHalfNum (int *numbers, int length) {
+    int result = numbers[0];
+    int count = 1;
+    
+    for (int i = 1; i < length; i++) {
+        if (count == 0) {
+            result = numbers[i];
+            count = 1;
+            continue;
+        }
+        if (result == numbers[i]) {
+            count++;
+        } else {
+            count--;
+        }
+    }
+    
+    return count;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
