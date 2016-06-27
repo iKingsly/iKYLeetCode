521f437b82001b2aff794be08f96b26f7afdd50b
郑钦洪 <369309988@qq.com>
2016-06-27 22:18:06 +0800

    顺序栈

--- 数据结构/4 栈和队列/4.4 顺序栈/4.4 顺序栈/main.cpp
+++ 数据结构/4 栈和队列/4.4 顺序栈/4.4 顺序栈/main.cpp
@@ -7,7 +7,38 @@
 //
 
 #include <iostream>
+#define MAXSIZE 5
+#define OK 1
+#define TRUE 1
+#define ERROR 0
+#define FALSE 0
+typedef int Status;
+typedef int SElemType;
 
+typedef struct {
+    // 数据段
+    SElemType data[MAXSIZE];
+    // 栈顶
+    int top;
+}SqStack;
+
+Status Push(SqStack *s, SElemType e) {
+    if (s->top == MAXSIZE - 1) {
+        return ERROR;
+    }
+    s->top++;
+    s->data[s->top] = e;
+    return OK;
+}
+
+Status Pop(SqStack *s, SElemType *e) {
+    if (s->top == -1) { // 栈为空
+        return ERROR;
+    }
+    *e = s->data[s->top];
+    s->top--;
+    return OK;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
