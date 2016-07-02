19d6d6ee479d529bff2f24fb4d5b52a6910877ff
郑钦洪 <369309988@qq.com>
2016-07-02 23:08:18 +0800

    求1＋2+。。。＋n不用循环处理

--- 剑指offer:面试题46:求1+2+...+n/剑指offer:面试题46:求1+2+...+n/main.cpp
+++ 剑指offer:面试题46:求1+2+...+n/剑指offer:面试题46:求1+2+...+n/main.cpp
@@ -7,9 +7,19 @@
 //
 
 #include <iostream>
+typedef unsigned int (*fun) (unsigned int);
+unsigned int Solution3_Teminator (unsigned int n) {
+    return 0;
+}
+unsigned int Sum_Solution3 (unsigned int n) {
+    static fun f[2] = {Solution3_Teminator, Sum_Solution3};
+    // 只有当n ＝ 0的时候 才会运行Solution3_Teminator 这样做到了递归退出的效果 
+    return n + f[!!n](n - 1);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    unsigned int result = Sum_Solution3(100);
+    printf("%d \n", result);
     return 0;
 }
